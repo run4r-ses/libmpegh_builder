7696042e141c81d6829d68d5444a01da2ad7deac
run4r.ses <157366808+run4r-ses@users.noreply.github.com>
2025-12-17 12:40:57 +0300

    common: attempt fix CLI

--- patches/stripnewline.py
+++
@@ -1,8 +0,0 @@
-import sys
-import subprocess
-
-raw_cmd = sys.argv[1].strip()
-extra_flags = sys.argv[2].strip()
-clean_cmd = raw_cmd.rstrip(' \t\n\r\\')
-final_cmd = f"{clean_cmd} {extra_flags}"
-subprocess.run(final_cmd, shell=True, check=True)
