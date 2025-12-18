205eb79640659dd157c677758072954a85ac8137
run4r.ses <157366808+run4r-ses@users.noreply.github.com>
2025-12-18 22:09:51 +0300

    patches: new features

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+PLATFORM="${1:-all}"
 
 patches=(
-    "libmpegh/CMakeLists.txt patches/useobj_cmakelists.patch"
+    "libmpegh/CMakeLists.txt patches/useobj_cmakelists.patch all"
+    "libmpegh/test/impeghd_main.c patches/disable_stdoutbuf_wasm.patch wasm"
 )
 
 normalize_to_lf() {
@@ -9,15 +11,37 @@ normalize_to_lf() {
     if command -v dos2unix &> /dev/null; then
         dos2unix "$file" &> /dev/null
     else
-        perl -pe 's/\r\n|\n|\r/\n/g' "$file"
+        perl -pe 's/\r\n|\n|\r/\n/g' -i "$file"
     fi
 }
 
-for item in "${patches[@]}"; do
-    target_file=$(echo "$item" | awk '{print $1}')
-    patch_file=$(echo "$item" | awk '{print $2}')
+platform_matches() {
+    local wanted="$1"
+    local allowed="$2"
 
-    echo "Applying patch $patch_file to $target_file"
+    if [[ "$allowed" == "all" ]]; then
+        return 0
+    fi
+
+    IFS=',' read -ra platforms <<< "$allowed"
+    for p in "${platforms[@]}"; do
+        if [[ "$p" == "$wanted" ]]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+for item in "${patches[@]}"; do
+    read -r target_file patch_file platforms <<< "$item"
+
+    if ! platform_matches "$PLATFORM" "$platforms"; then
+        echo "Skipping $patch_file (platform: $platforms)"
+        continue
+    fi
+
+    echo "Applying patch $patch_file to $target_file (platform: $PLATFORM)"
 
     normalize_to_lf "$patch_file"
     normalize_to_lf "$target_file"
@@ -25,4 +49,4 @@ for item in "${patches[@]}"; do
     patch -p0 --fuzz=3 --ignore-whitespace "$target_file" < "$patch_file"
 done
 
-echo "All patches applied"
+echo "All applicable patches applied"
